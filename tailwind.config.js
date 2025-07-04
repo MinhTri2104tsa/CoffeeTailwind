3b62599bd793dd60e3149ef88249111cefaff0e9
Trí Nguyễn Minh <mtri2105@gamil.com>
2025-07-04 22:10:33 +0700

    Add featured product card and more product

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,12 +17,15 @@ module.exports = {
       },
       keyframes: {
         slideDown: {
-          '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' }
+          '0%': { transform: 'translateY(-100%)' }, '100%': { transform: 'translateY(0)' }
+        },
+        fadeIn: {
+          from: { opacity: '0' }, to: { opacity: '1' }
         }
       },
       animation: {
-        slideDown: 'slideDown 0.4s ease-in-out'
+        slideDown: 'slideDown 0.4s ease-in-out',
+        fadeIn: 'fadeIn 0.5s ease-in-out'
       },
       backgroundImage: {
         'slider-coffee': "url('./img/favicon/slider_coffee.jpg')"
