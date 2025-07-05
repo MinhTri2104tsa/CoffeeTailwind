f4a86198cb0bfbf6bfdb9f3bffefb785da907312
Trí Nguyễn Minh <mtri2105@gamil.com>
2025-07-05 21:19:55 +0700

    Add magazine

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px'
+      },
       fontFamily: {
         karla: ['Karla', 'sans-serif']
       },
